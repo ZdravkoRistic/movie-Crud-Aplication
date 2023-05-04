ecd7bd330ff1282779445597382240c83f9a6505
ZdravkoRistic <easycodemaster@gmail.com>
2023-05-04 14:47:42 +0200

    add db and select DOM elements

---
+++ main.js
@@ -0,0 +1,31 @@
+//SELECTORS
+const moviesView = document.querySelector('.moviesView');
+const addMoviesView = document.querySelector('.add-movies-view');
+const editMoviesView = document.querySelector('.edit-movies-view');
+
+
+//INPUTS
+const imageInput = document.querySelector('input[name="image"]');
+const titleInput = document.querySelector('input[name="title"]');
+const yearInput = document.querySelector('input[name="year"]');
+const categoryInput = document.querySelector('input[name="category"]');
+const rateInput = document.querySelector('input[name="rate"]');
+const searchInput = document.querySelector('#search');
+
+//EDIT INPUTS
+const eTitleInput = document.querySelector('input[name="etitle"]');
+const eYearInput = document.querySelector('input[name="eyear"]');
+const eCategoryInput = document.querySelector('input[name="ecategory"]');
+const eRateInput = document.querySelector('input[name="erate"]');
+
+
+//Buttons
+const addViewBtn = document.querySelector('#addViewBtn');
+const addButton = document.querySelector('#addBtn');
+const editButton = document.querySelector('#editBtn');
+
+
+
+
+
+
