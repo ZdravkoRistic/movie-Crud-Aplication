4862cf77d2a9db0d7685f8a2c21482871a52a43f
ZdravkoRistic <easycodemaster@gmail.com>
2023-05-05 20:27:28 +0200

    functions
    Add events and functions for display Movies

--- main.js
+++ main.js
@@ -25,7 +25,55 @@ const addButton = document.querySelector('#addBtn');
 const editButton = document.querySelector('#editBtn');
 
 
+//Events
+addViewBtn.addEventListener('click', () => {
+    moviesView.style.display = "none";
+    addMoviesView.style.display = "block";
+})
+addButton.addEventListener('click', addMovie);
+editButton.addEventListener('click', editMovie);
+searchInput.addEventListener('keyup', searchMovie);
 
+
+
+function displayEditForm() {
+    let indexOfMovie = this.getAttribute('data-index');
+    let selectedMovie = db[indexOfMovie]
+
+    eTitleInput.value = selectedMovie.title;
+    eCategoryInput.value = selectedMovie.category;
+    eYearInput.value = selectedMovie.year;
+    eRateInput.value = selectedMovie.rate;
+
+    editButton.setAttribute('data-index', indexOfMovie);
+    moviesView.style.display="none"
+    editMoviesView.style.display="block"
+}
+function displayList(filtered) {
+    let currentMovies = filtered || db;
+    let html = ``;
+    currentMovies.forEach((movie,index) => {
+        html += `
+            <div class="card col-sm-2" style="width: 18rem;">
+                <img class="card-img-top" src="${movie.image}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">${movie.title}</h5>
+                    <p class="card-text">${movie.year}</p>
+                    <p class="card-text">${movie.category}</p>
+                    <p class="card-text">${movie.rate}</p>
+                    <a class="btn btn-warning editBtn" data-index="${index}">Edit</a>
+                    <a class="btn btn-danger deleteBtn" data-index="${index}">Delete</a>
+                </div>
+           </div>   
+        `.trim()
+
+
+    })
+
+    moviesView.innerHTML = html;
+
+    
+}
 
 
 
